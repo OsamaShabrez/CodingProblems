56758438a6b653bae3a58952803239425ea47620
Muhmmad Osama Shabrez <osamashabrez@live.com>
2022-11-08 22:16:44 +0100

    refactor(ListNode): moved to its own file

--- LinkLists/LinkListFindMiddleNode.cs
+++ LinkLists/LinkListFindMiddleNode.cs
@@ -2,18 +2,6 @@
 
 public static class LinkListFindMiddleNode
 {
-    public class ListNode
-    {
-        private int _val;
-        public readonly ListNode? Next;
-
-        public ListNode(int val = 0, ListNode? next = null)
-        {
-            _val = val;
-            Next = next;
-        }
-    }
-
     #region LeetCode-https://leetcode.com/problems/middle-of-the-linked-list/
 
     // Runtime 89 ms Beats 89.99% Memory 38.2 MB Beats 6.77%
---
+++ LinkLists/ListNode.cs
@@ -0,0 +1,13 @@
+namespace LinkLists;
+
+public class ListNode
+{
+    public int val;
+    public ListNode? Next;
+
+    public ListNode(int value = 0, ListNode? next = null)
+    {
+        val = value;
+        Next = next;
+    }
+}
