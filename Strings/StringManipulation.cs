a6b51f7189fbd4ff6193e31cb794681a21a3ef29
Muhmmad Osama Shabrez <osamashabrez@live.com>
2022-11-10 23:45:43 +0100

    feat(Strings): RemoveDuplicates

--- Strings/StringManipulation.cs
+++ Strings/StringManipulation.cs
@@ -4,18 +4,58 @@ namespace Strings;
 
 public static class StringManipulation
 {
-    #region LeetCode-https://leetcode.com/problems/make-the-string-great/
+    #region LeetCode-https: //leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+
+    // Runtime 83 ms Beats 99.4% Memory 46.1 MB Beats 16.75%
+    public static string RemoveDuplicates(string s)
+    {
+        var stack = new Stack<char>(s.Length);
+        foreach (var c in s)
+            if (stack.Count == 0)
+            {
+                stack.Push(c);
+            }
+            else
+            {
+                if (stack.Peek() == c)
+                    stack.Pop();
+                else
+                    stack.Push(c);
+            }
+
+        return new string(stack.Reverse().ToArray());
+    }
+
+    // Runtime 215 ms Beats 46.41% Memory 37.8 MB Beats 94.74%
+    public static string RemoveDuplicatesMemory(string s)
+    {
+        var sb = new StringBuilder(s);
+        for (var i = 0; i < sb.Length - 1; i++)
+        {
+            if (sb[i] != sb[i + 1]) continue;
+
+            sb.Remove(i, 2);
+            i -= i == 0 ? 1 : 2;
+        }
+
+        return sb.ToString();
+    }
+
+    #endregion
+
+    #region LeetCode-https: //leetcode.com/problems/make-the-string-great/
 
     // Runtime 85 ms Beats 92.4% Memory 38 MB Beats 43.36%
-    public static string MakeTheStringGreat(string s) {
+    public static string MakeTheStringGreat(string s)
+    {
         if (s.Length is 0 or 1) // empty or 1 char
             return s;
-        
+
         var sb = new StringBuilder(s);
         for (var i = 0; i < sb.Length - 1; i++)
         {
             if (Math.Abs(sb[i] - sb[i + 1]) != 32) continue;
-            
+
             sb.Remove(i, 2);
             i = -1;
         }
--- UnitTests/StringsTests.cs
+++ UnitTests/StringsTests.cs
@@ -6,10 +6,23 @@ namespace UnitTests;
 public class StringsTests
 {
     [Theory]
+    [InlineData("abbaca", "ca")]
+    [InlineData("azxxzy", "ay")]
+    [InlineData("s", "s")]
+    public void RemoveDuplicatesTest(string str, string output)
+    {
+        var result = StringManipulation.RemoveDuplicatesMemory(str);
+        Assert.Equal(result, output);
+
+        result = StringManipulation.RemoveDuplicates(str);
+        Assert.Equal(result, output);
+    }
+
+    [Theory]
     [InlineData("leEeetcode", "leetcode")]
     [InlineData("abBAcC", "")]
     [InlineData("s", "s")]
-    public void MakeTheStringGreat(string str, string output)
+    public void MakeTheStringGreatTest(string str, string output)
     {
         var result = StringManipulation.MakeTheStringGreat(str);
         Assert.Equal(result, output);
