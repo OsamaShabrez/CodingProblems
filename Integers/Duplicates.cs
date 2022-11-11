4144e2f157e281dc9a37fa2c3dae353dc7c4f1d7
Muhmmad Osama Shabrez <osamashabrez@live.com>
2022-11-11 23:50:34 +0100

    feat(Integers): RemoveDuplicates

---
+++ Integers/Duplicates.cs
@@ -0,0 +1,30 @@
+namespace Integers;
+
+public static class Duplicates
+{
+    #region LeetCode-https: //leetcode.com/problems/remove-duplicates-from-sorted-array
+
+    public static int RemoveDuplicates(int[] nums)
+    {
+        if (nums.Length < 2) return nums.Length;
+
+        var uniqueIdx = 1;
+        foreach (var v in nums)
+            if (nums[uniqueIdx - 1] != v)
+                nums[uniqueIdx++] = v;
+        return uniqueIdx;
+    }
+
+    public static int RemoveDuplicatesFirst(int[] nums)
+    {
+        var uniqueIdx = 0;
+        var uniqueVal = int.MinValue;
+        for (var i = 0; i < nums.Length; i++)
+            if (nums[i] != uniqueVal)
+                nums[uniqueIdx++] = uniqueVal = nums[i];
+
+        return uniqueIdx;
+    }
+
+    #endregion
+}
