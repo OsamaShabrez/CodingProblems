07391d18133820871f19164c232dbb60ef697d51
Muhmmad Osama Shabrez <osamashabrez@live.com>
2023-03-08 13:41:47 +0500

    feat(Arrays): 875. koko eating bananas

--- Arrays/KthValues.cs
+++ Arrays/KthValues.cs
@@ -23,4 +23,45 @@ public static class KthValues
         return num - 1;
     }
     #endregion
+    
+    #region 875 https://leetcode.com/problems/koko-eating-bananas
+    public static int MinEatingSpeed(int[] piles, int h)
+    {
+        if (piles.Length == h)
+            return piles.Max();
+
+        long maxK = 0;
+        long sum = 0;
+        foreach (var pile in piles)
+        {
+            maxK = Math.Max(pile, maxK);
+            sum += pile;
+        }
+        var minK = Math.Max(sum / h, 1);
+
+        do
+        {
+            var mid = (minK + maxK) / 2;
+            if (SpeedTest(piles, h, mid))
+                maxK = mid;
+            else
+                minK = mid + 1;
+        } while (minK < maxK);
+
+        return (int) minK;
+    }
+
+    private static bool SpeedTest(IEnumerable<int> piles, int h, double testK)
+    {
+        foreach (var pile in piles)
+        {
+            h -= (int) Math.Ceiling(pile / testK);
+            if (h < 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    #endregion
 }
--- UnitTests/ArraysTests.cs
+++ UnitTests/ArraysTests.cs
@@ -42,4 +42,15 @@ public class ArraysTests
         var result = KthValues.FindKthPositive(num, k);
         Assert.Equal(output, result);
     }
+
+    [Theory]
+    [InlineData(new[] { 3, 6, 7, 11 }, 8, 4)]
+    [InlineData(new[] { 30, 11, 23, 4, 20 }, 5, 30)]
+    [InlineData(new[] { 30, 11, 23, 4, 20 }, 6, 23)]
+    [InlineData(new[] {332484035,524908576,855865114,632922376,222257295,690155293,112677673,679580077,337406589,290818316,877337160,901728858,679284947,688210097,692137887,718203285,629455728,941802184}, 823855818, 14)]
+    public void MinEatingSpeedTest(int[] piles, int h, int output)
+    {
+        var result = KthValues.MinEatingSpeed(piles, h);
+        Assert.Equal(output, result);
+    }
 }
